d33ef232392533ea9707433c56c852917888648e
thisishammo <hammondkakhayanga@gmail.com>
2023-07-13 08:32:13 -0700

    write()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
 */
 int main(void)
 {
-	const char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
+	char txt[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, txt, 59);
 	return (1);
 }
