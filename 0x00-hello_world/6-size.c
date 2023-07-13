521efc9f8f8c2c6f09ceae02f61f49db3365988c
thisishammo <hammondkakhayanga@gmail.com>
2023-07-13 07:25:15 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-/*
- * * main - prints the specified text
- * * Return: 0 means that it was successful otherwise not successful
- * */
+/**
+* main - prints the specified text
+* Return: 0 means that it was successful otherwise not successful
+*/
 int main(void)
 {
 	int ch = sizeof(char);
@@ -10,6 +10,7 @@ int main(void)
 	int long_int = sizeof(long int);
 	int llint = sizeof(long long int);
 	int ft = sizeof(float);
+
 	printf("Size of a char: %d byte(s)\n", ch);
 	printf("Size of an int: %d byte(s)\n", i);
 	printf("Size of a long int: %d byte(s)\n", long_int);
