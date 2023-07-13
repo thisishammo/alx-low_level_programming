a1ef65be82cd87e43abc9feded47b1295cda0061
thisishammo <hammondkakhayanga@gmail.com>
2023-07-13 06:58:03 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main function - prints the specified text
+* Return: 0 means that it was successful otherwise not successful
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
