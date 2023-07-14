0c158eda37a9e9e1f37e8431ff218a81a3e5d028
thisishammo <hammondkakhayanga@gmail.com>
2023-07-14 05:17:49 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,11 @@ int main(void)
 			{
 				putchar('0' + i);
 				putchar('0' + j);
-				if (i != '8' && j != '9')
+				if (i != 8 || j != 9)
+				{
 				putchar(',');
 				putchar(' ');
+				}
 			}
 		}
 	}
