020153431be9c9532c39f063c110615ad86f5a93
thisishammo <hammondkakhayanga@gmail.com>
2023-07-18 22:32:29 +0300

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,21 +1,5 @@
-#ifndef main_h
-#define main_h
-#include<unistd.h>
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-void print_alphabet(void)
-{
-	int i;
-	
-	for (i = 97; i <= 122; i++)
-	{
-		char a = (char)i;
-		
-		write(1, &a, 1);
-	}
-}
+int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -27,5 +11,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endif
