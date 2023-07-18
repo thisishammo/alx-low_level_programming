e15338452a53488f057e098fe9c40093866f518d
thisishammo <hammondkakhayanga@gmail.com>
2023-07-18 02:07:04 -0700

    2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,4 @@
-#include<unistd.h>
 #include<stdio.h>
-/**
-<<<<<<< HEAD
-* print_alphabet - prints alphabets
-* Return: void
-=======
-* print_alphabets - prints alphabets
-* Return: 0 means successful otherwise error
->>>>>>> 62b54ef561a71bf79cc0aacc8e43a8083ca8bba5
-*/
 void print_alphabet(void);
 /**
 * main - excutes code
@@ -17,31 +7,21 @@ void print_alphabet(void);
 int main(void)
 {
 	print_alphabet();
-	return 0;
+	return (0);
 }
+/**
+* print_alphabet - prints alphabets
+* Return: void always
+*/
 void print_alphabet(void)
 {
 	int i;
-	
+
 	for (i = 97; i <= 122; i++)
 	{
-			char b = (char)i;
-			
-			putchar(b);
+		char b = (char)i;
+
+		putchar(b);
 	}
 		putchar('\n');
 }
-<<<<<<< HEAD
-/**
-* main - executes the print_alphabet funtion
-* Return: 0 means successful
-*/
-int main(void)
-{
-	print_alphabet();
-	write(1, "\n", 1);
-	return (0);
-}
-=======
-
->>>>>>> 62b54ef561a71bf79cc0aacc8e43a8083ca8bba5
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,39 +1,4 @@
-#include<unistd.h>
-<<<<<<< HEAD
-/**
-* print_alphabet_x10 - prints alphabets x10
-* Return: void
-*/
-void print_alphabet_x10(void)
-{
-	int i = 97;
-	int a = 10;
-
-	for (a = 10; a >= 1; a++)
-	{
-	for (i = 97; i <= 122; i++)
-	{
-		char letter = (char)i;
-
-		write(1, &letter, 1);
-	}
-	}
-}
-/**
-* main - checks code
-* Return: 0 always
-*/
-int main(void)
-{
-	print_alphabet_x10;
-	write(1, "\n", 1);
-	return (0);
-=======
 #include<stdio.h>
-/**
-* print_alphabets - prints alphabets
-* Return: 0 means successful otherwise error
-*/
 void print_alphabet_x10(void);
 /**
 * main - excutes code
@@ -42,23 +7,26 @@ void print_alphabet_x10(void);
 int main(void)
 {
 	print_alphabet_x10();
-	return 0;
+	return (0);
 }
+/**
+* print_alphabet_x10 - prints alphabets 10x
+* Return: void always
+*/
 void print_alphabet_x10(void)
 {
 	int i;
-	
-	for (i=0; i<10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-		int a;
-		
-		for (a = 97;a <= 122; a++)
+	int a;
+
+	for (a = 97; a <= 122; a++)
 		{
-			char b=(char)a;
-			
+			char b = (char)a;
+
 			putchar(a);
 		}
 		putchar('\n');
 	}
->>>>>>> 62b54ef561a71bf79cc0aacc8e43a8083ca8bba5
 }
