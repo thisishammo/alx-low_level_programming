0231b2d4f47678459c117542f42559f6a3e58b42
thisishammo <hammondkakhayanga@gmail.com>
2023-07-19 09:32:52 +0300

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,17 +14,19 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d,\n", n);
+				printf("%d\n", n);
 			}
-			else printf("%d, ", n);
+			else
+				printf("%d, ", n);
 		}
 	}
 	for (n = n; n >= 98; n--)
 	{
 		if (n == 98)
-			{
-				printf("%d,\n", n);
-			}
-			else printf("%d, ", n);
+		{
+			printf("%d\n", n);
+		}
+		else
+			printf("%d, ", n);
 	}
 }
