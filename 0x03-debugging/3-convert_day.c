f0cec9e0311c94d37d81321195269a76c5cd475a
thisishammo <hammondkakhayanga@gmail.com>
2023-07-19 16:21:14 +0300

    Last

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -5,99 +5,48 @@
 * for leap year
 * @month: month in number format
 * @day: day of month
-* @year: year
 * Return: day of year
 */
 
-int convert_day(int month, int day, int year)
+int convert_day(int month, int day)
 {
-	switch (month)
-	{
-		case 2:
-			day = 31 + day;
-			break;
-		case 3:
-			if ((year % 4) == 0)
-			{
-				day = 60 + day;
-			}
-			else
-				day = 59 + day;
-			break;
-		case 4:
-			if ((year % 4) == 0)
-			{
-				day = 91 + day;
-			}
-			else
-				day = 90 + day;
-			break;
-		case 5:
-			if ((year % 4) == 0)
-			{
-				day = 121 + day;
-			}
-			else
-				day = 120 + day;
-			break;
-		case 6:
-			if ((year % 4) == 0)
-			{
-				day = 152 + day;
-			}
-			else
-				day = 151 + day;
-			break;
-		case 7:
-			if ((year % 4) == 0)
-			{
-				day = 183 + day;
-			}
-			else
-				day = 182 + day;
-			break;
-		case 8:
-			if ((year % 4) == 0)
-			{
-				day = 213 + day;
-			}
-			else
-				day = 212 + day;
-			break;
-		case 9:
-			if ((year % 4) == 0)
-			{
-				day = 244 + day;
-			}
-			else
-				day = 243 + day;
-			break;
-		case 10:
-			if ((year % 4) == 0)
-			{
-				day = 274 + day;
-			}
-			else
-				day = 273 + day;
-			break;
-		case 11:
-			if ((year % 4) == 0)
-			{
-				day = 305 + day;
-			}
-			else
-				day = 304 + day;
-			break;
-		case 12:
-			if ((year % 4) == 0)
-			{
-				day = 335 + day;
-			}
-			else
-				day = 334 + day;
-			break;
-		default:
-			break;
-	}
+    switch (month)
+    {
+        case 2:
+            day = 31 + day;
+            break;
+        case 3:
+            day = 59 + day;
+            break;
+        case 4:
+            day = 90 + day;
+            break;
+        case 5:
+            day = 120 + day;
+            break;
+        case 6:
+            day = 151 + day;
+            break;
+        case 7:
+            day = 181 + day;
+            break;
+        case 8:
+            day = 212 + day;
+            break;
+        case 9:
+            day = 243 + day;
+            break;
+        case 10:
+            day = 273 + day;
+            break;
+        case 11:
+            day = 304 + day;
+            break;
+        case 12:
+            day = 334 + day;
+            break;
+        default:
+            break;
+    }
     return (day);
 }
