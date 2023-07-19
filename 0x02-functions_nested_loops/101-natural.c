d18d6521279328acc1b7356c7e244793b2bd3a60
thisishammo <hammondkakhayanga@gmail.com>
2023-07-19 10:40:57 +0300

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - executes code
+* Return: 0 always
+*/
+int sum_mul(int n)
+{
+	int i, sum;
+	if (n < 1024)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			if ((i % 3) == 0 || (i % 5) == 0)
+			{
+				sum = sum + i;
+			}
+		}
+		printf("%d", sum-1);
+	}
+}
+int main(void)
+{
+	int ham;
+
+	scanf("%d", &ham);
+	sum_mul(ham);
+	return (0);
+}
