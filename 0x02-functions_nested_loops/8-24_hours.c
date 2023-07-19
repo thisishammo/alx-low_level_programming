f8aa5c848c0a74f368d0a787de3ad5e1ff63b174
thisishammo <hammondkakhayanga@gmail.com>
2023-07-19 08:19:19 +0300

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* jack_bauer - plays the song
+*
+* Return: Always void
+*/
 void jack_bauer(void)
 {
 	int s, h;
-	for (h = 0; s <= 23; s++)
+
+	for (h = 0; h <= 23; h++)
 	{
 	for (s = 0; s <= 59; s++)
 	{
-		int fdh = h/10;
-		int sdh = h%10;
-		int fds = s/10;
-		int sds = s%10;
+		int fdh = h / 10;
+		int sdh = h % 10;
+		int fds = s / 10;
+		int sds = s % 10;
+
 		_putchar(fdh + '0');
 		_putchar(sdh + '0');
 		_putchar(':');
