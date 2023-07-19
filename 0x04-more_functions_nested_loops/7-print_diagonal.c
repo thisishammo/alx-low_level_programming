9d2723f67b43fa22a2a714ec09dcfc787a4ee8bd
thisishammo <hammondkakhayanga@gmail.com>
2023-07-19 20:11:19 +0300

    Squares

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+*  print_diagonal - draws a diagonal
+* @n: dictates how long the diagonal should be
+* Return: void
+*/
+void print_diagonal(int n)
+{
+	int h, i;
+	/*
+	int spacing = n - 1;
+	*/
+	if (n > 0)
+	{
+		for (h = 1; h <= n; h++)
+		{
+			for (i = 1; i <= h; i++)
+			{
+				_putchar('\\');
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
