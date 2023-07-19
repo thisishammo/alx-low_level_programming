d5c277340aa26d5ef4432147f2bdfc82fb872bb6
thisishammo <hammondkakhayanga@gmail.com>
2023-07-19 07:52:36 +0300

    7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,15 @@
 int print_last_digit(int a)
 {
 	int last;
+
 	if (a < 0)
 	{
-		last = a%10;
-		last = (last-(2*last));
+		last = a % 10;
+		last = (last - (2 * last));
 		_putchar((char)last + '0');
 		return (last);
-	} else
-	last = a%10;
+	}
+	last = a % 10;
 	_putchar((char)last + '0');
 	return (last);
 }
