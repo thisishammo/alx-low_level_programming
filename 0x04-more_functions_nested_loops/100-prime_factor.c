8d126fa70f21deafd860174c1b2050496d9157d4
thisishammo <hammondkakhayanga@gmail.com>
2023-07-20 14:01:46 +0300

    Largest Prime Factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<inttypes.h>
+/**
+* main - executes code
+* Return: 0 always
+*/
+int main(void)
+{
+	int64_t i;
+	int64_t n = 612852475143;
+	int64_t large = 0;
+	int ih;
+	for (i = 2; i <= n; i++)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
+	large = i - 1;
+	printf("%"PRId64, large);
+	return (0);
+}
