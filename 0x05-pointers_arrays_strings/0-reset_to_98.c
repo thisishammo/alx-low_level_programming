333804e8754ecf3e4fcf4cb8c4da1bd9a7766cf6
thisishammo <hammondkakhayanga@gmail.com>
2023-07-23 10:56:54 +0300

    To 98!

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+* reset_to_98 - updates an int to 98
+* @n: integer to be updated
+*
+* Return: nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers, pointers, pointers!
