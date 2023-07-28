0398e4c43fefcee0c78a7b0f1533b416d6dba6f9
thisishammo <hammondkakhayanga@gmail.com>
2023-07-28 10:26:32 +0300

    ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,36 +1,22 @@
 #include"main.h"
 /**
-* rot13 - encrypted data using the ROT13
-* @s: string to be encrypted
-* Return: Pointer to s
+* rot13 - converts symbols to ROT13
+* @c: string to be converted
+* Return: Pointer c
 */
-char *rot13(char *s)
+char *rot13(char *c)
 {
-	int i, ascii;
+	int i, h;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char s1[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklmn";
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (h = 0; c[h] != '\0'; h++)
 	{
-		ascii = (int)(*(s + i));
-		if (ascii >= 65 && ascii <= 77)
+		for (i = 0; i < 52; i++)
+		if (*(c + h) == s[i])
 		{
-			*(s + i) = (char)(ascii + 13);
+			*(c + h) = s1[i];
 		}
-		else
-		if (ascii >= 78 && ascii <= 90)
-		{
-			*(s + i) = (char)(ascii - 13);
-		}
-		else
-		if (ascii >= 97 && ascii <= 109)
-		{
-			*(s + i) = (char)(ascii + 13);
-		}
-		else
-		if (ascii >= 110 && ascii <= 122)
-		{
-			*(s + i) = (char)(ascii - 13);
-		}
-
 	}
-	return (s);
+	return (c);
 }
