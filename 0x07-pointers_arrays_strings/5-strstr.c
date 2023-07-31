77118f4b9b940154c7a8ae444e9f6bd60552522b
thisishammo <hammondkakhayanga@gmail.com>
2023-08-01 01:50:16 +0300

    %!

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,34 +1,25 @@
-#include"main.h"
-#include<stdlib.h>
+#include "main.h"
 /**
-* _strstr - locates substring
-* @haystack: string to be used
-* @needle: string to be searched
-* Return: pointer or NULL if no string found
-*/
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, h, k = 0, n = 0;
-	char *string = (char*)malloc(100);
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
 
-	for (i = 0; *(haystack + i) != '\0'; i++)
-	{
-		for (h = 0; *(needle + h) != '\0'; h++)
+		while (*l == *p && *p != '\0')
 		{
-			if ((*needle + h) == *(haystack + i))
-			{
-				k++;
-			}
+			l++;
+			p++;
 		}
+
+		if (*p == '\0')
+			return (haystack);
 	}
-	while (*(needle + n) != '\0')
-	{
-		n++;
-	}
-	if (n != k)
-	{
-		return (NULL);
-	}
-	else
-		return (needle);
+	return (0);
 }
