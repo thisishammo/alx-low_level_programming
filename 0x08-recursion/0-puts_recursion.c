51b12b65e22392e9b669baa0b9f2a95f2f10b40e
thisishammo <hammondkakhayanga@gmail.com>
2023-08-02 14:11:02 +0300

    Puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,10 +8,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
+		_putchar('\n');
 		return;
 	}
-	else
-		_putchar(s);
-		_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
