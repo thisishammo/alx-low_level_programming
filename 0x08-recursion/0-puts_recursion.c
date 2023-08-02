cf60aa76843add25ee4241c3588360a36b8dd597
thisishammo <hammondkakhayanga@gmail.com>
2023-08-02 14:05:53 +0300

    Puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include"main.h"
-#include<stdio.h>
 /**
 * _puts_recursion - prints a string
 * @s: string to be printed
@@ -13,6 +12,6 @@ void _puts_recursion(char *s)
 		return;
 	}
 	else
-		_putchar(*s);
+		_putchar(s);
 		_puts_recursion(s + 1);
 }
