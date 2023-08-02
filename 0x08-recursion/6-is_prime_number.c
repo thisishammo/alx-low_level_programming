c60d4a40a7717b5a8e234069dbd8f4d1e36d3193
thisishammo <hammondkakhayanga@gmail.com>
2023-08-02 16:49:15 +0300

    Prime number?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,17 @@
 #include"main.h"
 /**
-* is_prime_number - determines whether a number is a prime
-* number
-* @n: number to be determined
-* Return: integer
-*/
+ * search - searches for prime numbers
+ * @m: number to be determined whether prime
+ * @p: number
+ * Return: 1 if prime 0 if not
+ */
 int search(int m, int p)
 {
 	if (p != m)
 	{
 		if ((m % p) != 0)
 		{
-			return search(m, p + 1);
+			return (search(m, p + 1));
 		}
 		else
 			return (0);
@@ -21,6 +21,12 @@ int search(int m, int p)
 		return (1);
 	}
 }
+/**
+* is_prime_number - determines whether a number is a prime
+* number
+* @n: number to be determined
+* Return: integer
+*/
 int is_prime_number(int n)
 {
 	if (!(n > 1))
