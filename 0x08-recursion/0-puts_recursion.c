d26447e2452edda855aa4bafb76e95b6c95a6bbc
thisishammo <hammondkakhayanga@gmail.com>
2023-08-02 13:56:00 +0300

    Strlen

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+* _puts_recursion - prints a string
+* @s: string to be printed
+* ReturnL: nothing
+*/
+void _puts_recursion(char *s)
+{
+	if(s)
+	{
+		_putchar(*s);
+		_puts_recursion(s++);
+	}
+}
