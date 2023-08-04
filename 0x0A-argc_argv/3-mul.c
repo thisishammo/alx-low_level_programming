e218e7e46cc1f81806f0a085ad7adb70af1d154c
thisishammo <hammondkakhayanga@gmail.com>
2023-08-04 13:52:43 +0300

    Multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,11 @@ int main(int argc, char *argv[])
 	int i, product = 1;
 
 	if (argc > 1)
-	{	
+	{
 		for (i = 1; i < argc; i++)
 		{
 			product *= atoi(argv[i]);
-		}	
+		}
 		printf("%d\n", product);
 	}
 	else
