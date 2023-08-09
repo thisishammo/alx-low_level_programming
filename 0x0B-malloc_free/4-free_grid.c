dd0ab33080009635db29aa33dde6b194345f54ec
thisishammo <hammondkakhayanga@gmail.com>
2023-08-09 05:13:43 +0300

    Free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,17 +2,17 @@
 #include<stdlib.h>
 /**
  * free_grid - frees up a grid
- * @grid - grid to be freed
+ * @grid: grid to be freed
  * @height: height of grid
  * Return: nothing
  */
-void free_grid(int **grid, int height __attribute__((unused)))
+void free_grid(int **grid, int height)
 {
 	int i;
 
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)realloc(grid[i], 0);
+		free(grid[i]);
 	}
 	free(grid);
 }
