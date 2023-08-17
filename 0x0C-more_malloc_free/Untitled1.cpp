e79ef0b9bc9f7cd9ec7389fa1572cbc12187b66c
thisishammo <hammondkakhayanga@gmail.com>
2023-08-17 15:01:46 +0300

    Finished!

--- 0x0C-more_malloc_free/Untitled1.cpp
+++ 0x0C-more_malloc_free/Untitled1.cpp
@@ -1,23 +1,7 @@
 #include<stdio.h>
-#include<stdlib.h>
-long long unsigned int _atoi(char *s)
-{
-	int i, ascii, d = 0;
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		ascii = (int)s[i];
-		if (ascii >= 48 && ascii <= 57)
-		{
-			d += ascii - 48;
-			d *= 10;
-		}
-	}
-	d /= 10;
-	return d;
-}
 int main()
 {
-	long long unsigned int i = _atoi("1234567890");
-	printf("%llu\n", i);
+	char c = 'c';
+	printf("%x",99);
 	return 0;
 }
