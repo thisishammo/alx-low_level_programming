fb1f25f1495a78257748c62fe5c59461c589378b
thisishammo <hammondkakhayanga@gmail.com>
2023-08-25 01:29:21 +0300

    Count elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"lists.h"
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: list
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++)
+	{
+	}
+	return (i);
+}
