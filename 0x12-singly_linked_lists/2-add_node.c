979ae4591ea15d7fda477ce01c3992e2fcdc0295
thisishammo <hammondkakhayanga@gmail.com>
2023-08-25 02:05:40 +0300

    Add nodes at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,11 @@
 #include"lists.h"
 #include<stdlib.h>
 #include<string.h>
+/**
+ * _strlen - determines the length of a string
+ * @str: string to be determined
+ * Return: string length
+ */
 unsigned int _strlen(const char *str)
 {
 	unsigned int i = 0;
@@ -11,9 +16,17 @@ unsigned int _strlen(const char *str)
 	}
 	return (i);
 }
+/**
+ * add_node - adds nodes at the beginning of a singly
+ * linked list
+ * @head: address of the head node
+ * @str: string to be copied
+ * Return: address of the new added node
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *d = (list_t *)malloc(sizeof(list_t));
+
 	d->len = _strlen(str);
 	d->str = strdup(str);
 	d->next = *head;
